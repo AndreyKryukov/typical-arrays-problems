519b84901170ac3991ce598643d56666947dc371
AndreyKryukov <ander.kryukov@yandex.ru>
2021-02-27 14:14:32 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+exports.min = function min(array) {
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+  }
+}
+
+exports.max = function max(array) {
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+  }
+}
+
+exports.avg = function avg(array) {
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  } else {
+    return array.reduce((a, b) => (a + b)) / array.length;
+  }
+}
